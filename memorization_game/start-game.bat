a58f1f214e0b4f20b8163485750367f3ddb930c2
NeuralEquation <jamjam114514@outlook.jp>
2026-06-29 13:20:38 +0900

    Delete memorization_game directory

--- memorization_game/start-game.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-set "SCRIPT=%~dp0start-game.ps1"
-if not exist "%SCRIPT%" (
-  echo start-game.ps1 が見つかりません。
-  pause
-  exit /b 1
-)
-powershell -NoProfile -ExecutionPolicy Bypass -File "%SCRIPT%"
